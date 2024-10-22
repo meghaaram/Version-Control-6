5e3731a3da6251b8cca45a61b81d35993be7ce91
meghaaram <megha.ramprasad0@gmail.com>
2024-10-22 17:44:39 -0400

    setup

--- encode.py
+++ encode.py
@@ -1,8 +1,9 @@
+# encode by megha ramprasad
+
 def encode(password):
     pass
 
 def main():
-    keep = True
     password = ' '
     while True:
         print('Menu')
@@ -11,7 +12,15 @@ def main():
         print('2. Decode')
         print('3. Quit')
         print(' ')
-    option = print(input('Please enter an option: '))
-    if option == 1:
-        password = input('Please enter a password: ')
+        option = print(input('Please enter an option: '))
+        if option == 3:
+            break
+        elif option == 1:
+            str = input('Please enter a password to encode: ')
+            password = encode(str)
+            print('Your password has been encoded and stored!')
+        elif option == 2:
+            print('The encoded password is', password, 'and the original password is', decode(password))
+
+
 
