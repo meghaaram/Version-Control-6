69b30e9b3a1e7814534747a72b38eddd6939af1c
meghaaram <megha.ramprasad0@gmail.com>
2024-10-22 17:33:56 -0400

    setup

---
+++ encode.py
@@ -0,0 +1,17 @@
+def encode(password):
+    pass
+
+def main():
+    keep = True
+    password = ' '
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print(' ')
+    option = print(input('Please enter an option: '))
+    if option == 1:
+        password = input('Please enter a password: ')
+
