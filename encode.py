44b6e03374ef5e728cd468398a9f2176e40104f5
meghaaram <megha.ramprasad0@gmail.com>
2024-10-22 22:04:51 -0400

    testing

--- encode.py
+++ encode.py
@@ -1,27 +1,28 @@
 # encode by megha ramprasad
 
-def encode(encode_pw):
-    encode_pw = ''
-    for item in range(len(encode_pw)):
-        str = x wysjs
+def encode(password):
+    string = ''
+    for item in password[0:9]:
+        string += str((int(item[0]) + 3) % 10)
+    return str(string)
 
 
-
-def main():
-    password = ' '
-    while True:
-        print('Menu')
-        print('-------------')
-        print('1. Encode')
-        print('2. Decode')
-        print('3. Quit')
-        print(' ')
-        option = print(input('Please enter an option: '))
-        if option == 3:
-            break
-        elif option == 1:
-            str = input('Please enter a password to encode: ')
-            password = encode(str)
-            print('Your password has been encoded and stored!')
-        elif option == 2:
-            print('The encoded password is', password, 'and the original password is', decode(password))
+if __name__ == "__main__":
+        password = ''
+        while True:
+            print(' ')
+            print('Menu')
+            print('-------------')
+            print('1. Encode')
+            print('2. Decode')
+            print('3. Quit')
+            print(' ')
+            option = int(input('Please enter an option: '))
+            if option == 1:
+                password = input('Please enter a password to encode: ')
+                password = encode(password)
+                print('Your password has been encoded and stored!')
+            elif option == 2:
+                print('The encoded password is '+ str(password) + ', and the original password is ' + str(decode(password) + '.'))
+            elif option == 3:
+                break
